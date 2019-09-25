6e6e40704d683f43d4f3cd851096106a8e84424c
Dylan Zhao <dylan.zhao@epita.fr>
2019-09-25 20:31:41 +0200

    working

--- src/Game/PlayerIA/DayzlunPlayer.php
+++ src/Game/PlayerIA/DayzlunPlayer.php
@@ -9,14 +9,60 @@ use Hackathon\Game\Result;
  * @author Robin
  *
  */
+
+function getWinningChoice($choice)
+{
+  if ($choice == 'paper')
+    return 'scissors';
+  else if ($choice == 'scissors')
+    return 'rock';
+  else
+    return 'paper';
+}
+
 class DayzlunPlayer extends Player
 {
     protected $mySide;
     protected $opponentSide;
     protected $result;
 
+
+
     public function getChoice()
     {
+      $mylastchoice = $this->result->getLastChoiceFor($this->mySide);
+      $hislastchoice = $this->result->getLastChoiceFor($this->opponentSide);
+
+      $mylastScore = $this->result->getLastScoreFor($this->mySide);
+      $hislastScore = $this->result->getLastScoreFor($this->opponentSide);
+
+      $mychoices = $this->result->getChoicesFor($this->mySide);
+      $hischoices = $this->result->getChoicesFor($this->opponentSide);
+
+      $stats = $this->result->getStats();
+      $mystats = $this->result->getStatsFor($this->mySide);
+      $hisStats = $this->result->getStatsFor($this->opponentSide);
+
+      $nbRound = $this->result->getNbRound();
+
+      if ($mylastScore > 1)
+        return $mylastchoice;
+      else 
+        return getWinningChoice($mylastchoice);
+
+      
+      //print($mylastScore);
+      //print($stats);
+
+      
+     // print("\n");
+   /*   print($hislastchoice) ;
+      print($hislastchoice) ;
+      print($hislastchoice) ;
+      print($hislastchoice) ;*/
+      //$this->prettyDisplay();  
+
+
         // -------------------------------------    -----------------------------------------------------
         // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
         // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
@@ -26,9 +72,6 @@ class DayzlunPlayer extends Player
         // -------------------------------------    -----------------------------------------------------
         // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
         // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-       // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
         // -------------------------------------    -----------------------------------------------------
         // How to get the stats                ?    $this->result->getStats()
         // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
